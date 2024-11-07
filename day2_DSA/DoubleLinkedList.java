7a3a17d9b7280ee11a29eb16167ce8d7e7d3ed02
ManojKumar <manojkumarbhagavatula@gmail.com>
2024-11-07 10:48:11 +0530

    added DoubleLL

---
+++ day2_DSA/DoubleLinkedList.java
@@ -0,0 +1,140 @@
+package day2_DSA;
+
+class Node {
+    int data;
+    Node prev;
+    Node next;
+    Node(int data) {
+        this.data = data;
+    }
+}
+public class DoubleLinkedList {
+    Node head;
+    Node last = head;
+    int size;
+    boolean flip = false;
+    public void add(int data){
+        size++;
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            last = newNode;
+        }
+        else{
+           last.next = newNode;
+           newNode.prev = last;
+           last = newNode;
+        }
+    }
+    public void add(int position, int data){
+        Node newNode = new Node(data);
+        if(position < 1 || position > size){
+            System.out.println("Enter correct position");
+            return;
+        }
+        
+        else if(position == size)add(data);
+        else if(position == 0)addBegin(data);
+        else{
+            Node temp = head;
+            for(int i = 1; i<position-1; i++){
+                temp = temp.next;
+            }
+            newNode.next = temp.next;
+            temp.next.prev = newNode;
+            temp.next = newNode;
+            newNode.prev = temp;
+        }
+        size++;
+
+    }
+    public void addBegin(int data){
+        size++;
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+        }
+        else{
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+    }
+    public void pop(){
+        if(head == null){
+            System.out.println("Nothing to pop !!");
+            return;
+        }        
+        size++;
+        last = last.prev;
+        last.next = null;
+        
+    }
+   
+    public void popBegin(){
+        if(head == null){
+            System.out.println("Nothing to pop!!");
+            return;
+        }
+        head = head.next;
+        head.prev = null;
+        size--;
+
+    }
+    public void pop(int position){
+        if(position < 1 || position > size){
+            System.out.println("Enter correct position");
+        }
+        
+        else if(position == size)pop();
+        else if(position == 0)popBegin();
+        else{
+            Node temp = head;
+            for(int i = 1; i<position; i++){
+                temp = temp.next;
+            }
+            temp.next.prev = temp.prev;
+            temp.prev.next = temp.next;
+        }
+
+    }
+    public void reverse(){
+        if(head == null){
+            System.out.println("no elements to reverse");
+            return;
+        }
+        Node temp;
+        temp = head;
+        head = last;
+        last = temp;
+        flip = true;
+    }
+    public void displayElements(){
+        Node temp = head;
+        if(flip == true){
+            while(temp!=null){
+                System.out.print(temp.data+" -> ");
+                temp=temp.prev;
+            }
+            System.out.println("null");
+            flip = false;
+        }
+        else{
+            while(temp!=null){
+                System.out.print(temp.data+" -> ");
+                temp=temp.next;
+            }
+            System.out.println("null");
+        }
+    }
+    public static void main(String[] args) {
+        DoubleLinkedList dl = new DoubleLinkedList();
+        dl.add(1);
+        dl.add(2);
+        dl.add(3);
+        dl.add(4);
+        dl.displayElements();
+        dl.pop(3);
+        dl.displayElements();
+    }
+}
